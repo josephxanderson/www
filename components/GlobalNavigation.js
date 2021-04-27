fb650121379b8d2f6bedb19978fde36c9518dc4e
Joseph Anderson <josephxanderson@icloud.com>
2021-04-27 10:11:05 -0400

    Building out navigation.

--- components/GlobalNavigation.js
+++ components/GlobalNavigation.js
@@ -4,33 +4,37 @@ const GlobalNavigation = () => {
 	return (
 		<nav className="GlobalNavigation">
 			<div className="GlobalNavigation-Container">
-				<h3 className="GlobalNavigation-Title">⌘ Joseph Anderson</h3>
+				<h4 className="GlobalNavigation-Title">
+					<Link href="/">josephanderson.world 🌎</Link>
+				</h4>
 
 				<ul className="GlobalNavigation-List">
-					<li className="GlobalNavigation-Item">
-						<Link className="GlobalNavigation-Item-Link" href="/about">
+					<li className="GlobalNavigation-List-Item">
+						<Link className="GlobalNavigation-List-Item-Link" href="/about">
 							<span className="GlobalNavigation-Item-Content">About</span>
 						</Link>
 					</li>
 
-					<li className="GlobalNavigation-Item">
+					<li className="GlobalNavigation-List-Item">
 						<Link className="GlobalNavigation-Item-Link" href="/journal">
 							<span className="GlobalNavigation-Item-Content">Journal</span>
 						</Link>
 					</li>
 
-					<li className="GlobalNavigation-Item">
-						<Link className="GlobalNavigation-Item-Link" href="/Projects">
+					<li className="GlobalNavigation-List-Item">
+						<Link className="GlobalNavigation-List-Item-Link" href="/Projects">
 							<span className="GlobalNavigation-Item-Content">Projects</span>
 						</Link>
 					</li>
 
-					<li className="GlobalNavigation-Item">
-						<Link className="GlobalNavigation-Item-Link" href="/resume">
+					<li className="GlobalNavigation-List-Item">
+						<Link className="GlobalNavigation-List-Item-Link" href="/resume">
 							<span className="GlobalNavigation-Item-Content">Résumé</span>
 						</Link>
 					</li>
 				</ul>
+
+				<h4 className="GlobalNavigation-CommandButton">⌘</h4>
 			</div>
 		</nav>
 	);
