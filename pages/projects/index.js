207607e9d53c71e65a2e06af6417d19619fa5c8d
Joseph Anderson <josephxanderson@icloud.com>
2021-04-29 18:00:42 -0400

    Working on projects.

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,10 +4,22 @@ import PageHeader from '../../components/PageHeader';
 
 const Index = () => {
 	return (
-		<Layout title="Projects">
+		<Layout title="Projects by Joseph Anderson">
 			<PageHeader heading="Projects" />
 
-			<h3>PATCO Companion</h3>
+			<div className="Block">
+				<div className="Block-Image">
+					<figure className="Block-Image" style={{backgroundImage: "url('/images/projects/patco-companion/preview-image.jpg')"}}></figure>
+				</div>
+
+				<div className="Block-Header">
+					<h3 className="Block-Header-Heading">PATCO Companion</h3>
+				</div>
+
+				<div className="Block-Container">
+					
+				</div>
+			</div>
 		</Layout>
 	);
 }
